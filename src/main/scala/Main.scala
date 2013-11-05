b3ea8d637f46eda229f8bfe75283648be04fe4cf
porst17 <stussak@mfo.de>
2013-11-05 18:41:23 +0100

    initial source commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
+import scalafx.stage.Stage
+
+object Main extends JFXApp {
+  stage = new JFXApp.PrimaryStage {
+    title = "Hello World"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LIGHTGREEN
+      content = new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when (hover) choose Color.GREEN otherwise Color.RED
+      }
+    }
+  }
+}
